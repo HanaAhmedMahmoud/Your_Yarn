06f8775b30fa111c021735726c2a164df3294518
psyha9 <psyha9@exmail.nottingham.ac.uk>
2024-06-28 20:49:55 +0100

    added log out function

--- YourYarn/ViewModels/SettingsViewViewModel.swift
+++ YourYarn/ViewModels/SettingsViewViewModel.swift
@@ -5,12 +5,47 @@
 //  Created by Hana Mahmoud on 15/06/2024.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class SettingViewViewModel: ObservableObject{
+    
+    @Published var user: User? = nil
+    @Published var errorMessage = "Failed to log out"
+    
     init() {}
     
+    
+    func fetchUser(){
+        guard let userID = Auth.auth().currentUser?.uid else{
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users").document(userID).getDocument{ [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else{
+                return
+            }
+            
+            DispatchQueue.main.async{
+                self?.user = User(id: data["id"] as? String ?? "",
+                                  username: data["username"] as? String ?? "",
+                                  email: data["email"] as? String ?? "",
+                                  joined: data["joined"] as? TimeInterval ?? 0)
+            }
+        }
+    }
+    
     func logout() {
+        do{
+            try Auth.auth().signOut()
+        }catch{
+            print(errorMessage)
+        }
+        
+    }
+    
+    func deleteAccount(){
         
     }
 }
--- YourYarn/Views/SettingsView.swift
+++ YourYarn/Views/SettingsView.swift
@@ -21,80 +21,86 @@ struct SettingsView: View {
                     
                     VStack{
                         
-                        
-                        Text("Settings")
-                            .bold()
-                            .font(.title)
-                            .foregroundStyle(Color(red: 0.3569, green: 0.0039, blue: 0.4824))
-                            .padding(.all)
-                        
-                        
-                        
-                        // Info: Name, email, edit account
-                        VStack(alignment: .leading){
-                            Grid(alignment: .leadingFirstTextBaseline,
-                                 horizontalSpacing: 100,
-                                 verticalSpacing: 10){
-                                GridRow{
-                                    Text("Username: ")
-                                        .bold()
-                                    Text("Hana ")
-                                }
-                                
-                                GridRow{
-                                    Text("Email: ")
-                                        .bold()
-                                    Text("Hana ")
-                                }
-                                
-                                GridRow{
-                                    Text("Member Since: ")
-                                        .bold()
-                                    Text("Hana ")
-                                }
-                                
-                            }
-                        }.padding(.all)
-                        
-                        
-                        
-                        VStack
-                        {
-                            // sign out
-                            Button(action: {
-                                viewModel.logout()
-                            }) {
-                                Text("Log out")
-                                    .frame(maxWidth: .infinity, minHeight: 50)
-                                    .background(Color(red: 0.5255, green: 0.4157, blue: 0.6157))
-                                    .foregroundColor(.white)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.white, lineWidth: 1)
-                                    )
-                                
-                            }
+                        if let user = viewModel.user {
+                            Text("Settings")
+                                .bold()
+                                .font(.title)
+                                .foregroundStyle(Color(red: 0.3569, green: 0.0039, blue: 0.4824))
+                                .padding()
                             
-                            // delete account
-                            Button(action: {
-                                // code to delete account
-                            }) {
-                                Text("Delete account")
-                                    .frame(maxWidth: .infinity, minHeight: 50)
-                                    .background(Color(red: 0.7137, green: 0.051, blue: 0.0471))
-                                    .foregroundColor(.white)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.gray, lineWidth: 1)
-                                    )
-                            }
-                        }.padding(.horizontal, geometry.size.width * 0.1)
+                            
+                            
+                            // Info: Name, email, edit account
+                            VStack(alignment: .leading){
+                                Grid(alignment: .leadingFirstTextBaseline,
+                                     horizontalSpacing: 10,
+                                     verticalSpacing: 15){
+                                    GridRow{
+                                        Text("Username: ")
+                                            .bold()
+                                        Text("@" + user.username)
+                                    }
+                                    
+                                    GridRow{
+                                        Text("Email: ")
+                                            .bold()
+                                        Text(user.email)
+                                    }
+                                    
+                                    GridRow{
+                                        Text("Member Since: ")
+                                            .bold()
+                                        Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                                    }
+                                    
+                                }
+                            }.padding()
+                            
+                            
+                            
+                            VStack
+                            {
+                                // sign out
+                                Button(action: {
+                                    viewModel.logout()
+                                }) {
+                                    Text("Log out")
+                                        .frame(maxWidth: .infinity, minHeight: 50)
+                                        .background(Color(red: 0.5255, green: 0.4157, blue: 0.6157))
+                                        .foregroundColor(.white)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.white, lineWidth: 1)
+                                        )
+                                    
+                                }.padding(.top)
+                                
+                                // delete account
+                                Button(action: {
+                                    viewModel.deleteAccount()
+                                }) {
+                                    Text("Delete account")
+                                        .frame(maxWidth: .infinity, minHeight: 50)
+                                        .background(Color(red: 0.7137, green: 0.051, blue: 0.0471))
+                                        .foregroundColor(.white)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.gray, lineWidth: 1)
+                                        )
+                                }
+                            }.padding(.horizontal, geometry.size.width * 0.1)
+                            
+                            
+                            Spacer()
+                        }else{
+                            Text("Loading profile...")
+                        }
                         
-                        
-                        Spacer()
                     }
+                }.onAppear{
+                    viewModel.fetchUser()
                 }
                 
             }
